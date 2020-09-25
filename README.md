c99ec0c6f55a301d839dd99a59075807a2cbe46c
Jared Ticotin <jared_m_t@hotmail.com>
2020-09-24 23:35:31 -0400

    gameboard is set up and started adding event listeners for the guess letters

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hangman
-This game was made using pure HTML/CSS/Javascript. It is just a fun and simple game of hangman with different categories to choose from.
+This game runs on pure HTML/CSS/Javascript. It is just a fun and simple game of hangman with different categories to choose from and thousands of possible names/sentences/phrases.
 
-## data sets
-The data used in this game was extracted using puppeteer via nodeJS. All data was saved as a .json file so I could keep this project in the front-end and could easily be copied for anyone interested.
+## Data Sets
+The data used in this game was extracted using puppeteer via nodeJS. 
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+
+//sets up the game
 async function setGame(){
     const gameData = await (await fetch('./categories/gameData.JSON')).json();
     const categories = Object.keys(gameData);
@@ -18,13 +20,18 @@ async function setGame(){
 
     //choose sentence
     const sentence = gameData[category][0];//for now
+    /*const sentence = `here is a: sentence-words/symbols`;/* testing phrases that contain symbols*/
     console.log(sentence);
+    localStorage.setItem('sessionInfo',JSON.stringify({
+        'category':0,
+        'sentence':0
+    }));//save the phrase of this current session to local storage
     const words = sentence.split(" ");
 
     //display sentence
     const sentenceEl = document.querySelector('.gameboard-sentence');
     
-
+    //cycle through words to set up the game board
     for (word of words){
         var space = document.createElement('div');
         space.className='gameboard-nonletter';
@@ -34,8 +41,17 @@ async function setGame(){
         newword.className='gameboard-word';
         for(letter of word){
             let newletter = document.createElement('div');
-            newletter.className='gameboard-letter';
-            newletter.innerText=letter.toUpperCase();
+            
+
+            if (letter.toUpperCase().match('[A-Z]')){
+                newletter.className='gameboard-letter gameboard-letter-unknown';
+                newletter.innerText=letter.toUpperCase();//actual letter
+                newletter.innerText='_';
+            }else{
+                newletter.className='gameboard-nonletter';
+                newletter.innerHTML=letter;
+            }
+            
             newword.append(newletter);
         }
         sentenceEl.append(newword);
@@ -48,4 +64,36 @@ async function setGame(){
 }
 
 
-setGame();
+//add all event listeners
+document.addEventListener('DOMContentLoaded',async e=>{
+    await setGame();//setGame();//start game immediately
+
+
+    //add events to all the letters to choose from
+    await document.querySelectorAll('.letter').forEach(L=>{
+        
+        L.addEventListener('click',async eL=>{
+            await checkLetter(eL);
+        });
+    });
+
+
+
+
+});
+
+
+
+
+//game started. user clicked a letter
+async function checkLetter(e){
+    //get game data
+    const sessionInfo=await JSON.parse(localStorage.getItem('sessionInfo'));
+    const gameData = await (await fetch('./categories/gameData.JSON')).json();
+    const categories = Object.keys(gameData);
+    const category=categories[sessionInfo.category];
+    const sentence = gameData[category][sessionInfo.sentence];
+    //console.log(sentence);
+    const letterGuessed = e.target.innerText;
+    console.log(letterGuessed);
+}
