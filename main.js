fc8dddd544d97f2d7e71ecee9930eca3d3191bb0
Jared Ticotin <jared_m_t@hotmail.com>
2020-09-25 23:29:21 -0400

    added events for correct and wrong guesses. added max number of guesses

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 
+const maxWrongGuesses = 6;
+
+
+
+
+
 
 //sets up the game
 async function setGame(){
@@ -18,13 +24,18 @@ async function setGame(){
     //choose category
     const category = categories[0];//for now
 
+    //choose category index at random
+    //const cati = Math.floor(Math.random()*gameData[category].length);
+    const cati = Math.floor(Math.random()*100);
+
     //choose sentence
-    const sentence = gameData[category][0];//for now
+    const sentence = gameData[category][cati];//for now
     /*const sentence = `here is a: sentence-words/symbols`;/* testing phrases that contain symbols*/
-    console.log(sentence);
+    //console.log(sentence);//gives answer away
     localStorage.setItem('sessionInfo',JSON.stringify({
         'category':0,
-        'sentence':0
+        'sentence':cati,
+        'wrongGuesses':0
     }));//save the phrase of this current session to local storage
     const words = sentence.split(" ");
 
@@ -64,6 +75,78 @@ async function setGame(){
 }
 
 
+
+
+
+
+
+//game started. user clicked a letter
+async function checkLetter(e){
+    //get game data
+    const sessionInfo=await JSON.parse(localStorage.getItem('sessionInfo'));
+    const gameData = await (await fetch('./categories/gameData.JSON')).json();
+    const categories = Object.keys(gameData);
+    const category=categories[sessionInfo.category];
+    const sentence = gameData[category][sessionInfo.sentence].toUpperCase();
+
+    if (sessionInfo.wrongGuesses>=maxWrongGuesses){
+        //current game is over
+        return;
+    }
+
+    
+    //console.log(sentence);
+    const letterGuessed = e.target.innerText;
+    
+    //console.log(`guessed: ${letterGuessed}, correct: ${sentence}`);
+    const lettersEl = document.querySelectorAll('.gameboard-letter');
+    //console.log(Array.from(lettersEl).map(x=>x.innerText));//all current letters
+    var guessedCorrect = false;
+    const sentenceAnswer = [...sentence.matchAll('[A-Z]')].map(x=>x[0]);
+
+    //cycle through each letter of known answer and currently displayed answer
+    //check if it matches the guessed letter
+    sentenceAnswer.forEach((letterAnswer,i)=>{
+        //console.log(letterAnswer);
+        if (letterAnswer==letterGuessed){
+            lettersEl[i].innerText=letterAnswer;//display correct answer
+            lettersEl[i].className='gameboard-letter';
+            e.target.className='letter letter-pressed';
+            guessedCorrect=true;
+        }
+
+    });
+
+    //if no correct letter found, then mark as wrong
+    if (!guessedCorrect){
+
+        if (e.target.className.indexOf('letter-wrong')>=0){
+            //already wrongfully guessed this. no action needed
+            return;
+        }
+
+
+        e.target.className='letter letter-wrong';
+
+        //check if max guesses reached
+        sessionInfo.wrongGuesses++;
+        await localStorage.setItem('sessionInfo',JSON.stringify(sessionInfo));
+        if (sessionInfo.wrongGuesses>=maxWrongGuesses){
+            console.log('LOSER!!!!');
+        }
+    }
+
+    //check if user won
+    if (!document.querySelector('.gameboard-letter-unknown')){
+        console.log('win!!!!');
+    }
+
+
+}
+
+
+
+
 //add all event listeners
 document.addEventListener('DOMContentLoaded',async e=>{
     await setGame();//setGame();//start game immediately
@@ -77,23 +160,4 @@ document.addEventListener('DOMContentLoaded',async e=>{
         });
     });
 
-
-
-
 });
-
-
-
-
-//game started. user clicked a letter
-async function checkLetter(e){
-    //get game data
-    const sessionInfo=await JSON.parse(localStorage.getItem('sessionInfo'));
-    const gameData = await (await fetch('./categories/gameData.JSON')).json();
-    const categories = Object.keys(gameData);
-    const category=categories[sessionInfo.category];
-    const sentence = gameData[category][sessionInfo.sentence];
-    //console.log(sentence);
-    const letterGuessed = e.target.innerText;
-    console.log(letterGuessed);
-}
